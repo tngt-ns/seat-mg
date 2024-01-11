52c6c6e3eb0c6e614ce97adba4e81e21b5fd1ce9
谷口信人 <no-bu@taniguchinobuhitonoMacBook-Pro.local>
2024-01-11 23:35:14 +0900

    feat: データベースの追加

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///seat.db"
+# initialize the app with the extension
+db.init_app(app)
+
+class Seat(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    status: Mapped[str] = mapped_column(String, nullable=False, default="available")
+
+
+# class Seat(db.Model):
+#     id: db.Column(db.Integer, primary_key=True)
+#     isAvailable: db.Column(db.Boolean, default=True)
 
 @app.route("/")
 def index():
-    isDisable = request.args.get("isDisable", 1)
-    return render_template("index.html", isDisable=isDisable)
-
-@app.route("/admin")
+    return render_template("index.html")
+    
+    
+@app.route("/admin", methods=["GET", "POST"])
 def admin():
-    return render_template("admin.html")
+    if request.method == "GET":
+        seats = Seat.query.all()
+        return render_template("admin.html", seats=seats)
 
-@app.route("/visitor")
+@app.route("/visitor", methods=["GET", "POST"])
 def visitor():
-    return render_template("visitor.html")
+    if request.method == "GET":
+        seats = Seat.query.all()
+        return render_template("visitor.html", seats=seats)
 
 if __name__ == "__main__":
-    app.run()  
+    app.run()
