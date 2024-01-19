e4632a4b0c1f218cb6eb394686946718fdab44f3
谷口信人 <no-bu@taniguchinobuhitonoMacBook-Pro.local>
2024-01-20 01:06:59 +0900

    feat: 実際のレイアウト画像で実装

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from app import app, db, Seat
 
-num_seats = 4
+num_seats = 34
 
 # Flaskアプリケーションコンテキストを手動で確立
 with app.app_context():
@@ -9,12 +9,21 @@ with app.app_context():
     # データベースのテーブルを再作成
     db.create_all()
     
+    
+positions = [[155, 102, 21], [155, 158, 21], [155, 251, 21], [155, 307, 21], [155, 394, 21], [155, 450, 21],
+            [263, 121, 21], [340, 121, 21], [231, 312, 21], [231, 397, 21], [346, 312, 21], [347, 397, 21],
+            [436, 92, 21], [437, 176, 21], [552, 92, 21], [553, 176, 21], [437, 320, 21], [516, 320, 21],
+            [436, 441, 21], [516, 441, 21], [670, 113, 21], [750, 113, 21], [670, 236, 21], [750, 236, 21],
+            [520, 549, 21], [602, 435, 21], [723, 428, 21], [723, 482, 21], [723, 557, 21], [723, 611, 21],
+            [723, 685, 21], [723, 739, 21], [607, 685, 21], [606, 739, 21]]
 
-area_positions = ["76, 84, 40", "220, 84, 40", "76, 227, 40", "220, 227, 40"]
-button_positions = ["top: 44px; left: 36px; width: 80px; height: 80px;",
-                    "top: 44px; left: 180px; width: 80px; height: 80px;",
-                    "top: 187px; left: 36px; width: 80px; height: 80px;",
-                    "top: 187px; left: 180px; width: 80px; height: 80px;"]
+area_positions = []
+button_positions = []
+for i in range(num_seats):
+    pos = positions[i]
+    area_positions.append(str(pos[0]) + ", " + str(pos[1]) + ", " + str(pos[2]))
+    button_positions.append("top: " + str(pos[1] - pos[2]) + "px; left: " + str(pos[0] - pos[2]) + "px; width: " + str(pos[2] * 2) + "px; height: " + str(pos[2] * 2) + "px;")
+
 
 # テーブルが作成された後にデータを挿入
 with app.app_context():
