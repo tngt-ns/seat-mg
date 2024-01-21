a3626667a31287f20a39da55794f7ce1568d4153
so-hey <soh.tngt@gmail.com>
2024-01-21 18:31:56 +0900

    feat: リセットボタンの実装

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,44 @@
+window.onload = function () {
+    // 画像要素を取得
+    let container = document.querySelector(".seat-map-container");
+    console.log(container);
+
+    let style = window.getComputedStyle(container);
+    containerWidth = parseInt(style.width, 10);
+    console.log(containerWidth);
+
+    // 画面の幅を取得
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+
+    let windowSize = Math.min(windowWidth, windowHeight);
+    console.log(windowSize);
+
+    let scale = Math.min(containerWidth / windowSize, 0.6);
+    console.log(scale);
+    container.style.transform = "scale(" + scale + ")";
+    container.style.transformOrigin = "top left";
+
+    console.log(style.transform);
+};
+
 function updateSeatStatus(seatId) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "/update_seat", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
     // リクエストデータの作成
-    let data = JSON.stringify({ seat_id: seatId });
+    let data = JSON.stringify({ seat_id: seatId, status: "occupied" });
 
     xhr.onload = function () {
         if (xhr.status === 200) {
             let seatButton = document.getElementById("seat" + seatId);
-            seatButton.classList.toggle("available");
-            seatButton.classList.toggle("occupied");
+            if (seatButton.classList.contains("available")) {
+                seatButton.classList.replace("available", "occupied");
+            } else {
+                seatButton.classList.replace("occupied", "available");
+                data.status = "available";
+            }
             console.log("Seat status updated successfully");
         } else {
             console.error("Request failed. Status: " + xhr.status);
@@ -53,26 +81,45 @@ function updateSeats() {
     xhr.send();
 }
 
-window.onload = function () {
-    // 画像要素を取得
-    let container = document.querySelector(".seat-map-container");
-    console.log(container);
+function resetSeatsStatus() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/get_seats", true);
 
-    let style = window.getComputedStyle(container);
-    containerWidth = parseInt(style.width, 10);
-    console.log(containerWidth);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let seats = JSON.parse(xhr.responseText);
+            for (let seatId = 0; seatId < seats.length; seatId++) {
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", "/update_seat", true);
+                xhr.setRequestHeader(
+                    "Content-Type",
+                    "application/json;charset=UTF-8"
+                );
 
-    // 画面の幅を取得
-    let windowWidth = window.innerWidth;
-    let windowHeight = window.innerHeight;
+                // リクエストデータの作成
+                let data = JSON.stringify({
+                    seat_id: seatId,
+                    status: "available",
+                });
 
-    let windowSize = Math.min(windowWidth, windowHeight);
-    console.log(windowSize);
+                xhr.onload = function () {
+                    let seatButton = document.getElementById("seat" + seatId);
+                    seatButton.classList.remove("available", "occupied");
+                    seatButton.classList.add("available");
+                };
 
-    let scale = Math.min(containerWidth / windowSize, 0.6);
-    console.log(scale);
-    container.style.transform = "scale(" + scale + ")";
-    container.style.transformOrigin = "top left";
+                xhr.onerror = function () {
+                    console.error("Request failed");
+                };
 
-    console.log(style.transform);
-};
+                xhr.send(data);
+            }
+        }
+    };
+
+    xhr.onerror = function () {
+        console.error("Request failed");
+    };
+
+    xhr.send();
+}
