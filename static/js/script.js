cb594aabaeeac89c52022d1877e61fb79743fd68
so-hey <soh.tngt@gmail.com>
2024-01-21 16:33:05 +0900

    fix: scaleの倍率を調整

--- static/js/script.js
+++ static/js/script.js
@@ -69,7 +69,7 @@ window.onload = function () {
     let windowSize = Math.min(windowWidth, windowHeight);
     console.log(windowSize);
 
-    let scale = Math.min(containerWidth / windowSize, 0.7);
+    let scale = Math.min((0.7 * containerWidth) / windowSize, 0.7);
     console.log(scale);
     container.style.transform = "scale(" + scale + ")";
     container.style.transformOrigin = "top left";
